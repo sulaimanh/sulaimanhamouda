359c008317d607282b725e3bf29ee31a25c0037d
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2021-02-12 01:32:54 -0500

    Take back some files

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,31 +1,12 @@
-import { useEffect, useState } from "react";
-
 import BlogContainer from "@/components/blog/blog-container";
 import Head from "next/head";
 import Layout from "@/components/layout";
 import fetcher from "@/lib/fetcher";
 import { getAllPosts } from "@/lib/api";
-import { useAuth } from "@/lib/hooks/useAuth";
 import { useQuery } from "react-query";
 
 export default function Index({ allPosts }) {
   const { data } = useQuery(["views"], () => fetcher("/api/views"));
-  const { user, loading } = useAuth();
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    if (!user) {
-      allPosts.forEach((post, index) => {
-        if (post.isSubmitted === "false") {
-          allPosts.splice(index, 1);
-        }
-      });
-    }
-
-    if (!loading) {
-      setPosts(allPosts);
-    }
-  }, [loading]);
 
   return (
     <>
@@ -33,7 +14,7 @@ export default function Index({ allPosts }) {
         <Head>
           <title>sulaiman | blog</title>
         </Head>
-        <BlogContainer views={data} sect='blog' allPosts={posts} />
+        <BlogContainer views={data} sect='blog' allPosts={allPosts} />
       </Layout>
     </>
   );
