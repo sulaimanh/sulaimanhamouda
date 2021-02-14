55895cb563b84c693d5e76afa1b95a2bb8fb5ccf
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2021-02-14 00:24:58 -0500

    Update mdx component

--- components/mdxComponents/tag.js
+++ components/mdxComponents/tag.js
@@ -1,6 +1,8 @@
 import { anchor as A, headingSecondary as H2 } from "@/components/text/text";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
 export default function Tag({ title, id }) {
@@ -8,10 +10,15 @@ export default function Tag({ title, id }) {
 
   return (
     <div id={id}>
-      <H2 className='hover:underline cursor-pointer'>
-        <Link href={`/blog/${router?.query.slug}/#${id}`}>
-          <A>{title}</A>
-        </Link>
+      <H2 className='title flex items-center hover:underline cursor-pointer w-max'>
+        <div className='mr-2'>
+          <Link href={`/blog/${router?.query.slug}/#${id}`}>
+            <A>{title}</A>
+          </Link>
+        </div>
+        <div className='paperclip hidden'>
+          <FontAwesomeIcon icon={faPaperclip} size='sm' color='bg-gray-100' />
+        </div>
       </H2>
     </div>
   );
