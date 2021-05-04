796808a9f0ceeb6eb5b39f583c46861b72e5b70a
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2021-05-04 17:40:48 -0400

    new page

---
+++ pages/resume/index.js
@@ -0,0 +1,62 @@
+import { anchor as A } from "@/components/text/text";
+import Button from "@/components/button/button";
+import Container from "@/components/container";
+import Head from "next/head";
+import Image from "@/components/mdxComponents/image";
+import Layout from "@/components/layout";
+
+export default function Resume() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>sulaiman | resume</title>
+        </Head>
+        <Container>
+          <div className='flex flex-col items-center'>
+            <div className='mb-5'>
+              <Button
+                color='black'
+                textColor='white'
+                hoverColor='white'
+                hoverBorderColor='black'
+                hoverTextColor='black'
+                handler={() => {
+                  window.location.href = "http://sulaimanhamouda/resume.pdf";
+                }}
+              >
+                Download Resume
+              </Button>
+            </div>
+            <div
+              className='shadow-md w-max cursor-pointer'
+              onClick={() => {
+                window.location.href = "http://sulaimanhamouda/resume.pdf";
+              }}
+            >
+              <Image
+                alt='Resume'
+                width='400'
+                height='500'
+                src='/assets/personal/resume.png'
+              />
+            </div>
+            <div className='mt-5'>
+              <A blue outerLink href='https://github.com/sulaimanh'>
+                GitHub
+              </A>
+              <A
+                className='ml-3'
+                blue
+                outerLink
+                href='https://github.com/sulaimanh'
+              >
+                LinkedIn
+              </A>
+            </div>
+          </div>
+        </Container>
+      </Layout>
+    </>
+  );
+}
